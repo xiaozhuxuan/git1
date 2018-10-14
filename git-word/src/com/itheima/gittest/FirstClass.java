76a65ce2768b6e3e96372c89b280d18f6775e818
xiaozhu <xiaozhu@itheima.com>
2018-10-14 19:23:22 +0800

    idea第一次提交工程

---
+++ git-word/src/com/itheima/gittest/FirstClass.java
@@ -0,0 +1,16 @@
+package com.itheima.gittest;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+        System.out.println("hello word!");
+        System.out.println("hello word!");
+        System.out.println("hello word!");
+
+
+
+
+
+
+    }
+}
